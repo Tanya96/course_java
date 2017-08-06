438a4c0260193769bff2013760be08213fa72d09
taty <andreeva-tanja@mail.ru>
2017-08-06 22:50:22 +0300

    рефакторинг testGroupCreate

--- addressbook/src/test/java/ru/tanya/GroupCreationTests.java
+++ addressbook/src/test/java/ru/tanya/GroupCreationTests.java
@@ -1,16 +1,11 @@
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+package ru.tanya;
 
-import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class GroupCreationTests {
     FirefoxDriver wd;
@@ -21,38 +16,60 @@ public class GroupCreationTests {
         wd = new FirefoxDriver();
         wd.manage().timeouts();
         wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) throws InterruptedException {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
         Thread.sleep(500);
     }
-    
+
     @Test
     public void testGroupCreation() throws InterruptedException {
-        wd.findElement(By.cssSelector("a[href='group.php']")).click();
-        Thread.sleep(500);
-        wd.findElement(By.id("content")).click();
-        Thread.sleep(500);
-        wd.findElement(By.name("new")).click();
-        Thread.sleep(500);
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("test1");
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("test2");
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("test3");
-        wd.findElement(By.name("submit")).click();
-        Thread.sleep(500);
+        goToGroupPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData("test1", "test2", "test3"));
+        submitGroupCreation();
+        returnGroupPage();
+    }
+
+    private void returnGroupPage() {
         wd.findElement(By.linkText("group page")).click();
     }
-    
+
+    private void submitGroupCreation() throws InterruptedException {
+        wd.findElement(By.name("submit")).click();
+        Thread.sleep(500);
+    }
+
+    private void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    private void initGroupCreation() throws InterruptedException {
+        wd.findElement(By.name("new")).click();
+        Thread.sleep(500);
+    }
+
+    private void goToGroupPage() throws InterruptedException {
+        wd.findElement(By.cssSelector("a[href='group.php']")).click();
+        Thread.sleep(500);
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
